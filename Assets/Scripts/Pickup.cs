2c00748b5c91bd6f4c0bff6e7a262f6a7195204e
LunalightLulu <mccormacklulu@gmail.com>
2024-01-18 03:31:45 +0000

    Pickups floatate and rotate.
    Added to Pickup script so that pickups now bob up and down in the air, and rotate slowly.

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -7,10 +7,42 @@ public class Pickup : MonoBehaviour
     [SerializeField] GameObject Player;
     [SerializeField] PlayerBoosts ProcessScript;
     [SerializeField] string Type;
+    [SerializeField] Vector3 RotationRate;
+    [SerializeField] Vector3 FlotationRate;
+    [SerializeField] Vector3 FlotationFluctuation;
+    private Vector3 MaxHeight;
+    private Vector3 MinHeight;
+    private bool FloatUp;
     private void Start()
     {
         Player = GameObject.FindGameObjectWithTag("Player");
         ProcessScript = Player.GetComponent<PlayerBoosts>();
+        MaxHeight = gameObject.transform.position + FlotationFluctuation;
+        MinHeight = gameObject.transform.position - FlotationFluctuation;
+    }
+    private void Update()
+    {
+        gameObject.transform.Rotate(RotationRate);
+        Float();
+    }
+    private void Float()
+    {
+        if (transform.position.y > MaxHeight.y)
+        {
+            FloatUp = false;
+        }
+        else if (transform.position.y < MinHeight.y)
+        {
+            FloatUp = true;
+        }
+        if (FloatUp)
+        {
+            transform.position += FlotationRate;
+        }
+        else if (!FloatUp)
+        {
+            transform.position -= FlotationRate;
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
